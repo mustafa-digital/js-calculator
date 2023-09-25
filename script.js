5e01cb101dc6352a1104918b93ac5282184f00e1
Mustafa <124535608+MUChappy@users.noreply.github.com>
2023-09-25 13:42:39 -0400

    Add math functions and operate() function in js

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 /* script.js */
+const ADD = "+";
+const SUBTRACT = "-";
+const MULTIPLY = "*";
+const DIVIDE = "/";
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operate(num1, operator, num2) {
+    switch(operator) {
+        case ADD:
+            return add(num1, num2);
+            break;
+        case SUBTRACT:
+            return subtract(num1,num2);
+            break;
+        case MULTIPLY:
+            return multiply(num1, num2);
+            break;
+        case DIVIDE:
+            return divide(num1, num2);
+            break;
+        default:
+            return "ERROR";
+    }
+}
+
+let num1;
+let num2;
+let operator;
